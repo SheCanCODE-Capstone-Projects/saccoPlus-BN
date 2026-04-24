3791b50470d7db96126cb98ba6b9835082601ba9
THERESE07 <niyigena.therese@gmail.com>
2026-04-24 08:21:35 +0200

    ft-added RegisterGroupRequest for group registration

---
+++ src/main/java/com/saccoplus/dto/request/RegisterGroupRequest.java
@@ -0,0 +1,18 @@
+package com.saccoplus.dto.request;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RegisterGroupRequest {
+
+    private String groupName;
+
+    private String representativeFirstName;
+    private String representativeLastName;
+    private String representativePhone;
+    private String password;
+
+    private List<MemberDto> members;
+}
