b9b2806f3aa52c5d6cebad76051c506d57a1ff37
lucie <imanisinginzwelucie@gmail.com>
2026-05-11 12:25:24 +0200

    feat: add findByEmail method to IndividualUserRepository

--- src/main/java/com/saccoplus/repository/IndividualUserRepository.java
+++ src/main/java/com/saccoplus/repository/IndividualUserRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.saccoplus.entity.IndividualUser;
 
+import java.util.Optional;
+
 public interface IndividualUserRepository extends JpaRepository<IndividualUser, Long> {
 
     boolean existsByPhone(String phone);
+    Optional<IndividualUser> findByEmail(String email);
 
 }
