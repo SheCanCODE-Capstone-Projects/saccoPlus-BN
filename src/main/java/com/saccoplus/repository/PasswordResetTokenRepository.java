1e7d68201fac520cc7e96e35626299d7dfa0f6ec
lucie <imanisinginzwelucie@gmail.com>
2026-05-11 12:01:25 +0200

    feat: add PasswordResetTokenRepository

--- src/main/java/com/saccoplus/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/saccoplus/repository/PasswordResetTokenRepository.java
@@ -1,4 +1,15 @@
 package com.saccoplus.repository;
 
-public interface PasswordResetTokenRepository {
+import com.saccoplus.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    Optional<PasswordResetToken> findByToken(String token);
+
+    void deleteByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
